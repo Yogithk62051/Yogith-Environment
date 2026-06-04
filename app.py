7381c685079b89ef96ee939bef2a67dc4b22b4fc
Yogith Bharadwaj <yogithk62051@gmail.com>
2026-06-04 22:10:24 +0530

    Removed delete users route

--- app.py
+++ app.py
@@ -403,6 +403,15 @@ def leaderboard():
         Team=Team
     )
 
+@app.route("/delete_all_users")
+def delete_all_users():
+
+    User.query.delete()
+
+    db.session.commit()
+
+    return "All users deleted successfully"
+
 
 
 
